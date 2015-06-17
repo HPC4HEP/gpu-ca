cca0a199ebdd7e3f2bc488c538f3a2bdbdd94ba7
Felice Pantaleo <felice.pantaleo@cern.ch>
2015-06-17 19:19:57 +0200

    directory structure

--- src/GPUCA.cu
+++ src/GPUCA.cu
@@ -10,8 +10,19 @@
 #include "include/Cell.h"
 #include "include/CUDAQueue.h"
 
+__global__ void singleBlockCA (Cell** arrayOfLayers, int numberOfLayers, int* numberOfCellsPerLayer )
+{
+
+
+
+}
+
+
+
 
 int main()
 {
+
+
 
 }
