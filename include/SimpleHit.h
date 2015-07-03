bac2246e8d74427860b3a54a42439eb22a804a74
felicepantaleo <felice.pantaleo@cern.ch>
2015-07-03 14:04:25 +0200

    Simple Hit for simple simulation of the CA

---
+++ include/SimpleHit.h
@@ -0,0 +1,18 @@
+/*
+ * SimpleHit.h
+ *
+ *  Created on: Jul 3, 2015
+ *      Author: fpantale
+ */
+
+#ifndef GPUCA_INCLUDE_SIMPLEHIT_H_
+#define GPUCA_INCLUDE_SIMPLEHIT_H_
+
+struct SimpleHit {
+	float phi, eta;
+	int layerId;
+};
+
+
+
+#endif /* GPUCA_INCLUDE_SIMPLEHIT_H_ */
