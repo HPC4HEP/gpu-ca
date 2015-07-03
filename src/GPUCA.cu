51e4c35f024d6c3959d7588fbda68b0ace0c5108
felicepantaleo <felice.pantaleo@cern.ch>
2015-07-03 14:05:04 +0200

    Simple simulation of the CA using Simple Hits has started the development.

--- src/GPUCA.cu
+++ src/GPUCA.cu
@@ -6,6 +6,8 @@
  */
 
 #include <cuda.h>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 #include "Cell.h"
 #include "CUDAQueue.h"
@@ -23,6 +25,31 @@ __global__ void singleBlockCA (Cell<20, c_numParameters>** arrayOfLayers, int nu
 
 int main()
 {
+	constexpr auto numLayers = 5;
+	constexpr auto numHits = 100;
+
+	srand (time(NULL));
+	std::pair<float, float> range_eta(0.1, 0.3);
+	std::pair<float, float> range_phi(0.5, 0.6);
+
+
+
+
+	for (auto i = 0; i< numLayers; ++i)
+	{
+		float tmp_eta = range_eta.first + (range_eta.second - range_eta.first)*(static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
+		float tmp_phi = range_phi.first + (range_phi.second - range_phi.first)*(static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
+
+	}
+
+
+	std::vector<SimpleHit> hitsVector(numHits);
+
+
+
+
+
+
 
 
 }
